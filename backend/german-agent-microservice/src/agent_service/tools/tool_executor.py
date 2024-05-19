92c8c062293da4f13d51d6cf7431effe4485ea04
ali <kholmovaia@students.uni-marburg.de>
2024-05-19 15:52:25 +0200

    feat tool_executor.py add execute

--- backend/german-agent-microservice/src/agent_service/tools/tool_executor.py
+++ backend/german-agent-microservice/src/agent_service/tools/tool_executor.py
@@ -5,6 +5,8 @@ from agent_service.core.pydantic_tool_exe import ToolExecutorConfigModel
 from pydantic import ValidationError
 import logging
 from typing import List
+# The `ToolExecutor` class is responsible for executing tools based on user input and configuration
+# settings (tool list).
 class ToolExecutor:
     def __init__(self) -> None:
         self.parse_config()
@@ -12,10 +14,33 @@ class ToolExecutor:
         self.tools: List[Tool] = self.factory.tools
         self.tool_names: List[str] = self.factory.tool_names
 
-    def execute(self, tool:Tool, input:str) -> str:
-        pass
+    def execute(self, tool_name:str, input:str) -> str:
+        '''The function executes a specified tool with a given input 
+        and returns the observation or anerror message if the tool or input is invalid.
+        
+        '''
+        try:
+            tool = self.get_tool_by_name(tool_name)
+            observation = tool.run(input)
+        except ValueError as e:
+            observation = "Invalid tool or tool input"
+        return observation
 
+    def get_tool_by_name(self, name:str)->Tool:
+        '''The function `get_tool_by_name` searches for a tool by name in a list of tools and returns the
+        tool if found.
+        
+        '''
+        for tool in self.tools:
+            if tool.name==name:
+                return tool
+        return ValueError
+    
     def parse_config(self):
+        '''The function `parse_config` reads settings from a configuration file and creates a
+        `ToolExecutorConfigModel` object.
+        
+        '''
         try:
             config = Config("tool-exe")
             settings = config.get_settings()
