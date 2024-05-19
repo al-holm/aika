279a3daeda7d3c9186f21112b22920daec833081
ali <kholmovaia@students.uni-marburg.de>
2024-05-19 16:38:11 +0200

    feat AgentFinalStep for final answers

--- backend/german-agent-microservice/src/agent_service/agent/agent_step.py
+++ backend/german-agent-microservice/src/agent_service/agent/agent_step.py
@@ -3,7 +3,6 @@ from typing import List, Any
 
 class AgentStep(BaseModel):
     thought:str
-    tool_names:List[str]
     action:str
     action_input:str
     observation:str=None
@@ -21,36 +20,17 @@ class AgentStep(BaseModel):
             res += "\nObservation: " + self.observation
         res += "\n"
         return res
+    
+class AgentFinalStep(BaseModel):
+    final_answer:str
 
-    @field_validator('thought')
-    def validate_thought(cls, value):
-        if ''==value:
-            raise ValueError('You should start with "Thought:" (think about what you should do next).')
-        return value
-    
-    @field_validator('action')
-    def validate_action(cls, value, info: dict[str, Any]):
-        if ''==value:
-            raise ValueError('No action were given.')
-        if value not in info.data["tool_names"]:
-            raise ValueError(f'An action name is incorrect, select an available tool: {info.data["tool_names"]}')
-        return value
-    
-    @field_validator('action_input')
-    def validate_action_input(cls, value):
-        if ''==value:
-            raise ValueError("Input shouldn't be empty.")
-        return value
-    
-    @field_validator('observation')
-    def validate_observation(cls, value):
-        if '' not in value:
-            raise ValueError("Observation shouldn't be empty.")
-        return value
-    
-    @field_validator('tool_names')
-    def validate_observation(cls, value):
-        if len(value)==0:
-            raise ValueError("Tools shouldn't be empty.")
-        return value
+    def toString(self):
+        """
+        The `toString` function in Python creates a formatted string representation of an object's
+        attributes.
+        """
+        res = "Final Answer: " + self.final_answer + "\n"
+        return res
+
+
 
