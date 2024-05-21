6ce5070a6f1c109ce5edf92ccf5c822b33e589d7
ali <kholmovaia@students.uni-marburg.de>
2024-05-21 13:51:20 +0200

    feat tools web search summarization

--- backend/german-agent-microservice/src/agent_service/tools/web_search_tool.py
+++ backend/german-agent-microservice/src/agent_service/tools/web_search_tool.py
@@ -1,22 +1,32 @@
 from agent_service.tools.tool import Tool
+from agent_service.prompts.task_generation_prompt import WEB_SUMMARY_TEMPLATE
+from agent_service.agent.llm import LLMBedrock
+from agent_service.prompts.prompt_builder import PromptBuilder
 import requests
 import os
 from time import sleep 
 
 class WebSearch(Tool):
+    PROMPT_ID = "web summary"
+    TEMPLATE = WEB_SUMMARY_TEMPLATE
     def __init__(self):
         self.name = "Web-Suche"
         self.description = "Hilfreich, wenn man Informationen im Internet nachschauen möchte."
         self.api_key = os.environ["RAPIDAPI_KEY"]
         self.api_host = "searxng.p.rapidapi.com"
         self.url = "https://searxng.p.rapidapi.com/search"
+        self.llm = LLMBedrock()
+        self.prompt = PromptBuilder()
+        self.prompt.create_prompts(
+            {self.PROMPT_ID : self.TEMPLATE}
+            )
 
     def run(self, input: str):
         """
         The function uses the requests library to search SearxNG for results based on the input string.
         """
-        answer = "Request time out"
-        
+        def_answer = "Request time out"
+        answer = def_answer
         querystring, headers = self.setup_request(input)
         requests_n = 0
         while requests_n < 10:
@@ -27,7 +37,12 @@ class WebSearch(Tool):
                 requests_n = 100
             except Exception:
                 sleep(2)
-            requests_n += 1           
+            requests_n += 1  
+        if answer != def_answer:
+            self.query = self.prompt.generate_prompt(name_id=self.PROMPT_ID, 
+                                                     text=input,
+                                                     search_results=answer)
+            answer = self.llm.run(self.query) 
         return answer
     
     def parse_results(self, results):
