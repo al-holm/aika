108dfcb79ac1a0d3dca007e52e309b41e9322ef9
Timofei Ushakov <tommccalester@gmail.com>
2024-05-22 14:45:39 +0200

    refactore: comments in agent_step.py, llm.py, reasoning_trace.py

--- backend/german-agent-microservice/src/agent_service/agent/reasoning_trace.py
+++ backend/german-agent-microservice/src/agent_service/agent/reasoning_trace.py
@@ -3,37 +3,46 @@ from agent_service.agent.agent_step import AgentStep, AgentValidationStep, Agent
 import json, uuid
 from pathlib import Path
 import os
-# The `ReasoningLogger` class represents a trace of reasoning steps with methods to add steps, store
-# exceptions, and generate a JSON output.
+
+
 class ReasoningLogger:
+    """
+    represents a trace of agent steps with methods to add steps, store
+    exceptions, and generate JSON output.
+    """
     def __init__(self) -> None:
         self.trace : List[Union[AgentStep, AgentValidationStep, AgentFinalStep]] = []
         self.query = None
         self.errors : List[Exception] = []
     
     def add_exception(self, e:Exception):
-        '''appends an Exception object to a list of errors.
-        
-        '''
+        """
+        adds an exception to the errors list
+        """
         self.errors.append(e)
     
     def add_step(self, step: Union[AgentStep, AgentValidationStep, AgentFinalStep]):
-        '''appends a step object to a list of steps.
-        
-        '''
+        """
+        adds a step to the trace list
+        """
         self.trace.append(step)
 
     def remove_step(self, index: int) -> None:
-        '''removes a step object from a list of steps.
+        """
+        removes a step from the trace list by its index.
         
-        '''
+        Parameters
+        ----------
+        index : int
+            an index that points to a step in the trace list
+        """
         if 0 <= index < len(self.trace):
             self.trace.pop(index)
 
     def get_last_step(self) -> Union[AgentStep, AgentValidationStep, AgentFinalStep]:
-        '''returns the last step object from a list of steps.
-        
-        '''
+        """
+        returns the last step object from the trace list.
+        """
         if self.trace:
             return self.trace[-1]
         return None
@@ -47,9 +56,9 @@ class ReasoningLogger:
         return res
 
     def to_json(self):
-        '''converts an object's attributes into a JSON format and writes it to a file.
-        
-        '''
+        """
+        converts the trace into JSON and writes it to the out file
+        """
         dict_f = {"query": self.query}
         dict_f["reasoning_steps"] = [step.model_dump() for step in self.trace]
         dict_f["final_answer"] = self.final_answer
@@ -62,16 +71,20 @@ class ReasoningLogger:
             json.dump(dict_f, file, indent=4, ensure_ascii=False)
     
     def set_query(self, query:str):
-        '''sets the query attribute of an object to the provided query string.
-               
-        '''
+        """
+        sets the query
+
+        Parameters
+        ----------
+        query : str 
+            the provided query
+        """
         self.query = query
 
     def build_final_answer(self):
-        '''finds the last instance of AgentStep in the step list,
-        creates a final step with the observation in this step as final answer, 
-        
-        '''
+        """
+        builds an AgentFinalStep instance with the observation of the last AgentStep instance as the final answer, 
+        """
         for i in reversed(self.trace):
             if isinstance(i, AgentStep):
                 step = AgentFinalStep(final_answer=i.observation)
