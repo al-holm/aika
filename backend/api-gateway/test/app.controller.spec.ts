90d7c80888c9d0b8dba7d12bcea5729d4db839e7
ali-ubuntu <kholmovaia@stundents.uni-marburg.de>
2024-05-23 14:06:11 +0200

    refactor api-gateway move tests

--- backend/api-gateway/test/app.controller.spec.ts
+++ backend/api-gateway/test/app.controller.spec.ts
@@ -1,6 +1,7 @@
+import { AppService } from './../src/app.service';
+import { AppController } from './../src/app.controller';
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 
 describe('AppController', () => {
   let appController: AppController;
