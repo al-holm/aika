e3c194757ae1c12d822a92c3e49cdec20c73e670
ali-ubuntu <kholmovaia@stundents.uni-marburg.de>
2024-05-24 15:35:13 +0200

    fix agent misc

--- backend/german-agent-microservice/src/main.py
+++ backend/german-agent-microservice/src/main.py
@@ -1,12 +1,14 @@
 #### FOR DEBUGGING PURPOSES
 from scripts.setup_logging import setup_logging
 from agent_service.agent.agent import Agent
-
-
-
+from agent_service.agent.task_type import TaskType
+from agent_service.core.config import Config
 
 if __name__ == "__main__":
     setup_logging()
-    a = Agent()
-    a.run("Übersetze für mich auf Russisch 'In Rahmen dieser Arbeit werden viel über KI erfahren.'.")
-    
+    llm = 'bedrock' # bedrock or runpod
+    task_type = TaskType.LESSON
+    Config.set_llm(llm, task_type)
+
+    a = Agent(task_type)
+    a.run("Bitte erstelle eine Grammatikerklärung mit Beispiele für das Thema 'Konjuktiv 2 (II)' und benutze danach ein Tool zur Aufgabenerstellung, damit du 5 Aufgaben erstellst.")
