559d0948baf4e9e0dc65e146b31234564ab02f19
Ali Kholmovaia <90838877+a-kholmovaia@users.noreply.github.com>
2024-05-29 12:22:23 +0200

    feat task init structure

---
+++ frontend/lib/task_widget/models/task.dart
@@ -0,0 +1,16 @@
+enum TaskType{
+  multipleChoice, fillTheGaps, openQuestion
+}
+
+class Task {
+  TaskType? type;
+  String? question;
+  List<String>? answerOptions;
+  List<String>? solutions;
+  List<String>? userAnswers;
+  bool completed = false; 
+
+  Task(this.type, this.question, this.answerOptions, this.solutions, this.userAnswers);
+
+  
+}
