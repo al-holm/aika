2debe15a68ada8e867242615c1700ab38457ced3
Ali Kholmovaia <90838877+a-kholmovaia@users.noreply.github.com>
2024-06-16 10:18:21 +0200

    feat: agent added docs asylverfahren

--- backend/agent-microservice/src/agent_service/tools/retrieval_tool.py
+++ backend/agent-microservice/src/agent_service/tools/retrieval_tool.py
@@ -2,7 +2,7 @@ from agent_service.tools.tool import Tool
 from agent_service.prompts.prompt_builder import PromptBuilder
 from agent_service.prompts.tool_prompt import RETRIEVER_TEMPLATE
 import os
-from typing import List, Dict, Tuple
+from typing import Dict
 import logging
 from pymilvus import MilvusClient
 from pymilvus.model import hybrid
@@ -26,16 +26,32 @@ class RetrievalTool(Tool):
         )
 
         self.min_chunk_len = 200
-        markdown_text_list = self.read_markdown_folder(self.DOC_PATH)
-        self.list_src, self.list_docs = self.parse_info(markdown_text_list)
+        self.max_chunk_len = 500
+        self.load_docs()
         self.ef = hybrid.BGEM3EmbeddingFunction(
-            model_name='BAAI/bge-m3', # Specify the model name
-            device='cpu', # Specify the device to use, e.g., 'cpu' or 'cuda:0'
-            use_fp16=False # Specify whether to use fp16. Set to `False` if `device` is `cpu`.
+            model_name='BAAI/bge-m3', 
+            device='cpu', 
+            use_fp16=False
         )
         self.start_vector_store(init)
         if init:
             self.add_docs()
+
+    def load_docs(self):
+        markdown_text_list = self.read_markdown_folder(self.DOC_PATH)
+        txt_text_list = self.read_txt_folder(self.DOC_PATH)
+        md_list_src, md_list_docs = self.parse_info(markdown_text_list, mode="md")
+        txt_list_src, txt_list_docs = self.parse_info(txt_text_list, mode="txt")
+        self.list_docs, self.list_src = [], []
+        self.list_docs.extend(md_list_docs)
+        self.list_docs.extend(txt_list_docs)
+        self.list_src.extend(md_list_src)
+        self.list_src.extend(txt_list_src)
+        self.get_stats_chunks(self.list_docs)
+        
+    def get_stats_chunks(self, list_docs):
+        len_list = [len(i) for i in list_docs]
+        logging.info(f"\nMean len docs: {sum(len_list)//len(len_list)}, Min: {min(len_list)}, Max: {max(len_list)}\n")
     
     def read_markdown_folder(self, folder_path):
         """
@@ -56,8 +72,24 @@ class RetrievalTool(Tool):
                 with open(file_path, 'r', encoding="utf-8") as f:
                     markdown_text_list.append(f.read())
         return markdown_text_list
+    
+    def read_txt_folder(self, folder_path):
+        """
+        Reads all txt files in a folder and saves the text in a list.
 
-    def parse_info(self, markdown_text_list):
+        Returns
+        -------
+            A list of strings, where each string is the text content of a txt file.
+        """
+        text_list = []
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".txt"):
+                file_path = os.path.join(folder_path, filename)
+                with open(file_path, 'r', encoding="utf-8") as f:
+                    text_list.append(f.read())
+        return text_list
+
+    def parse_info(self, text_list, mode="md"):
         """
         Parse a list of markdown texts into a DataFrame containing document information.
 
@@ -72,14 +104,17 @@ class RetrievalTool(Tool):
         """
         list_docs = []
         list_src = []
-        for text in markdown_text_list:
-            src_list, chunks = self.get_src_chunks(text)
+        for text in text_list:
+            if mode=="md":
+                src_list, chunks = self.get_src_chunks_md(text)
+            else:
+                src_list, chunks = self.get_src_chunks_txt(text)
             list_docs.extend(chunks)
             list_src.extend(src_list)
 
         return (list_src, list_docs)
     
-    def get_src_chunks(self, text: str):
+    def get_src_chunks_md(self, text: str):
         """
         """
         blocks = text.split("\n# ")
@@ -102,7 +137,20 @@ class RetrievalTool(Tool):
         return ([src for chunk in chunks], chunks) 
 
 
-        
+    def get_src_chunks_md(self, text: str):
+        src = text.split("URL: ")[1].split("\n")[0]
+        block = text.split("Body Text:\n")[1].split("Related:")[0]
+        chunks = []
+        chunk_buff = ''
+        if len(block) > self.max_chunk_len:
+            dots = [i for i, char in enumerate(block) if i == "."]
+            mid = len(block) // 2  
+            ind = min(dots, key=lambda x: abs(x-mid))
+            chunks = [block[:ind+1], block[ind+1:]]
+        else:
+            chunks = block
+        return ([src for chunk in chunks], chunks)
+
     
     def start_vector_store(self, init):
         """
@@ -148,10 +196,10 @@ class RetrievalTool(Tool):
         query_vectors = self.ef.encode_queries([input])['dense']
 
         results = self.client.search(
-            collection_name=self.COLLECTION_NAME, # target collection
-            data=query_vectors,                # query vectors
-            limit=2,                           # number of returned entities
-            output_fields=["text", "source"], # specifies fields to be returned
+            collection_name=self.COLLECTION_NAME,
+            data=query_vectors,               
+            limit=2,                          
+            output_fields=["text", "source"], 
         )
         parsed_result = self.parse_retrieval_results(results[0])
         self.query = self.prompt.generate_prompt(name_id=self.PROMPT_ID, 
