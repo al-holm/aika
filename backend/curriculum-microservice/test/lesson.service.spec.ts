460b3580f7dadf937e9611109d214609cd315a97
ali-ubuntu <kholmovaia@stundents.uni-marburg.de>
2024-06-22 11:06:19 +0200

    test curriculum coverage

--- backend/curriculum-microservice/test/lesson.service.spec.ts
+++ backend/curriculum-microservice/test/lesson.service.spec.ts
@@ -5,12 +5,26 @@ import { Lesson, LessonType } from '../src/interfaces/lesson.interface'
 import { TaskDto } from '../src/lesson/dto/task.dto';
 import { NotImplementedException } from '@nestjs/common';
 import { TaskType } from 'src/interfaces/task.interface';
+import { setupServer } from 'msw/node';
+import { http } from 'msw';
+export const server = setupServer(
+  http.post('http://127.0.0.1:5000/get_lesson', () => {
+    return new Response(JSON.stringify({text: 'lesson created' }), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  })
+);
 
-// Mocking the readLessonsFromFile function
+
+
+// Mocking the json utils funcitons
 jest.mock('../src/util/json.util', () => ({
   readLessonsFromFile: jest.fn(),
   writeLessonsToFile: jest.fn()
 }));
+
 
 describe('LessonService', () => {
   let service: LessonService;
@@ -35,6 +49,10 @@ describe('LessonService', () => {
     },
   ];
 
+  beforeAll(() => {
+    server.listen()
+  })  
+
   beforeEach(async () => {
     (readLessonsFromFile as jest.Mock).mockResolvedValue(mockLessons);
 
@@ -44,7 +62,7 @@ describe('LessonService', () => {
 
     service = module.get<LessonService>(LessonService);
     //@ts-ignore
-    await service.loadLessons(); // Ensure lessons are loaded before each test
+    await service.loadLessons();
   });
 
   it('should be defined', () => {
@@ -59,27 +77,53 @@ describe('LessonService', () => {
     //@ts-ignore
     const nextLesson = await service.getNextUncompletedLesson();
     expect(nextLesson).toEqual({
+      id: 1,
       type: 'Reading',
       topic: 'Read a short story about daily routines',
     });
   });
+  
+  it('should return the correct lesson grammar attribute', async () => {
+    const lesson = mockLessons[1];
+    //@ts-ignore
+    const topic = await service.getNextTopic('Grammar', lesson);
+    expect(topic).toEqual("Complex sentences and clauses");
+  });
 
-  it('should construct the correct request in getNextLesson', async () => {
-    const consoleSpy = jest.spyOn(console, 'log');
-    await service.getNextLesson();
-    expect(consoleSpy).toHaveBeenCalledWith('[Reading][Read a short story about daily routines][None][1][1][1]');
+  it('should return the correct lesson reading attribute', async () => {
+    const lesson = mockLessons[1];
+    //@ts-ignore
+    const topic = await service.getNextTopic('Reading', lesson);
+    expect(topic).toEqual("Read an article on climate change");
+  });
+
+  it('should return the correct lesson listening attribute', async () => {
+    const lesson = mockLessons[1];
+    //@ts-ignore
+    const topic = await service.getNextTopic('Listening', lesson);
+    expect(topic).toEqual("Listen to a news report");
+  });
+
+  it('should send the correct request in getNextLesson', async () => {
+    const consoleLogMock = jest.spyOn(console, 'log').mockImplementation();
+    const results = await service.getNextLesson();
+    expect(results).toEqual({
+      text:'lesson created',id:1, lessonType:'Reading'
+    })
+    expect(console.log).toHaveBeenCalledWith(
+      'Sending request to the agent: [Reading][Read a short story about daily routines][None][2][1][1]'  
+    )
   });
 
   it('should process user answers and mark lesson as completed', async () => {
-    const task: TaskDto = {
+    const task: TaskDto[] = [{
       type: TaskType.single_choice,
       id: 1,
       lessonType: 'Reading',
       question: 'What is your name?',
-      options: [['John', 'Doe']],
       userAnswers: [['John']],
       solutions: ['John'],
-    };
+    }];
 
     await service.processUserAnswers(task);
     const updatedLesson = service['lessons'].find(lesson => lesson.id === 1);
@@ -97,7 +141,7 @@ describe('LessonService', () => {
     await service.loadLessons();
     //@ts-ignore
     const nextLesson = await service.getNextUncompletedLesson();
-    expect(nextLesson).toEqual({ type: 'Grammar', topic: 'Konkunktiv II' });
+    expect(nextLesson).toEqual({id:-1, type: 'Grammar', topic: 'Konkunktiv II' });
   });
 
   it('should throw an error for unsupported lesson type in getNextTopic', () => {
