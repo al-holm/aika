412b45d74140a7b46a22ab971a56717bc81bf0f9
ali-ubuntu <kholmovaia@stundents.uni-marburg.de>
2024-06-23 12:11:05 +0200

    feat profiles - prompt finetuning

--- backend/agent-microservice/src/agent_service/tools/reading_generation_tool.py
+++ backend/agent-microservice/src/agent_service/tools/reading_generation_tool.py
@@ -6,14 +6,37 @@ class ReadingGenerator(Tool):
     """
     a tool for the reading text generation
     """
+    ROOT_DIR = 'agent_service/tools/res/profiles/'
     def __init__(self, name: str, description: str, llm: str, 
                     prompt_id: str, prompt_template: str, max_tokens:int) -> None:
         super().__init__(name, description, llm, prompt_id, prompt_template, max_tokens)
+        self.load_profiles()
+        self.current_profile = 'Layla'
 
     def run(self, input:str):
         """
         runs LLM with input. 
         """
-        self.query = self.prompt.generate_prompt(name_id=self.prompt_id, text=input)
+        name, story = self.get_story()
+        self.query = self.prompt.generate_prompt(
+            name_id=self.prompt_id, topic=input, 
+            person_name=name, person_story=story
+            )
         answer = self.llm.run(self.query)
         return answer
+    
+    def load_profiles(self):
+        with open(self.ROOT_DIR + 'mo.md', 'r') as f:
+            mo_story = f.readlines()
+        with open(self.ROOT_DIR + 'layla.md', 'r') as f:
+            layla_story = f.readlines()
+        self.profiles = {'Mo': mo_story, 'Layla' : layla_story}
+
+    def get_story(self):
+        story = self.profiles[self.current_profile]
+        name = self.current_profile
+        if  self.current_profile == 'Layla':
+            self.current_profile = 'Mo'
+        else:
+            self.current_profile = 'Layla'
+        return name, story
--- backend/agent-microservice/src/main.py
+++ backend/agent-microservice/src/main.py
@@ -4,21 +4,23 @@ from agent_service.agent.agent import Agent
 from agent_service.agent.task_type import TaskType
 from agent_service.core.config import Config
 from agent_service.prompts.trajectory_library import TrajectoryInjector
+from agent_service.prompts.tool_prompt import READING_TEMPLATE
 from agent_service.rag.rag import RAG
+from agent_service.tools.reading_generation_tool import ReadingGenerator
 if __name__ == "__main__":
     setup_logging()
     llm = 'bedrock' # bedrock or runpod
     task_type = TaskType.LESSON
     Config.set_llm(llm, task_type)
-    rt = RAG(init=True)
+    #rt = RAG(init=True)
     """ while True:
         print('\n\n')
         print(rt.run(input('\n\n\nType your question:'))) """
-    tj = TrajectoryInjector(True)
+    #tj = TrajectoryInjector(True)
     #print(tj.inject_trajectories('Wie kann ich sagen ich komme später.'))
-    a = Agent(task_type=task_type)
-
+    #a = Agent(task_type=task_type)
+    tool = ReadingGenerator('', '', llm, 'reading', READING_TEMPLATE, 350)
     query = "" 
     while query != "stop":
-        query = input("Your query: ")
-        a.run(query)
+        query = input("\n\nYour query: ")
+        print(tool.run(query))
