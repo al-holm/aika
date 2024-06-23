7476c5d41fbb00f7f8746e80ccde1652ae518252
Timofei Ushakov <tommccalester@gmail.com>
2024-06-23 18:26:23 +0200

    feat[agent-ms]: create lesson master
    To remove aika's reasoning from the lesson generation, I've created a
    new entity called LessonMaster which generates a lesson without
    LLM-reasoning loop

---
+++ backend/agent-microservice/src/agent_service/tools/lesson_master.py
@@ -0,0 +1,64 @@
+from agent_service.tools.tool_executor import ToolExecutor
+from agent_service.agent.task_type import TaskType
+from agent_service.parsers.exercises_parser import ExercisesParser
+from typing import Dict
+import re
+
+class LessonMaster:
+    
+    def __init__(self):
+        self.tool_executor = ToolExecutor(task_type=TaskType.LESSON)
+        self.exercises_parser = ExercisesParser()
+
+    def run(self, query: str):
+        """
+        creates a lesson unit
+
+        Parameters:
+        -----------
+        query: str
+            a string containing the type, topic, and number of exercises of the desired lesson
+            for instance, [Grammar][Präteritum][None][1][1][1]
+
+        Returns:
+        --------
+        lesson: Dict
+            a dictionary containing the text for the lesson and all exercises
+        """
+        
+        parsed_query = self.parse_query(query)
+
+        if parsed_query["type"] == "Grammar":
+            text = self.tool_executor.execute('Web-Suche', parsed_query["main-topic"] + " Erklärung mit Beispielen")
+            # should look like that: [Grammar][Präteritum][None][1][1][1][text]
+            task_generator_query = query + f"[{text}]"
+        else:
+            text = self.tool_executor.execute('Lesetext erstellen', parsed_query["main-topic"])
+            # should look like that: [Reading][2][0][1][text]
+            task_generator_query = f"[{parsed_query["type"]}][{parsed_query["single-choice"]}][{parsed_query["gap-filling"]}][{parsed_query["open-ended"]}][{text}]"
+
+        raw_lesson = self.tool_executor.execute('Deutschaufgaben generieren', task_generator_query)
+        print(f"raw_lesson: \n{raw_lesson}")
+        lesson = self.exercises_parser.parse(raw_lesson)
+        return lesson
+    
+    def parse_query(self, query: str) -> Dict[str, str]:
+        """
+        parses a query of the form
+        [Type][Main-grammar-topic][Secondary-grammar-topic][Single-Choice-Questions-Number][Gaps-filling-exercises-number][Open-Questions-Number]
+        
+        Returns:
+        --------
+        parsed_query: Dict[str, str]
+        """
+        matches = re.findall(r'\[(.*?)\]', query, re.DOTALL)
+
+        parsed_query = {}
+        parsed_query["type"] = matches[0]
+        parsed_query["main-topic"] = matches[1]
+        parsed_query["secondary-topic"] = matches[2]
+        parsed_query["single-choice"] = matches[3]
+        parsed_query["gap-filling"] = matches[4]
+        parsed_query["open-ended"] = matches[5]
+
+        return parsed_query
