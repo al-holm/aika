ab5f92b83e0d97081c1c4a530403847beffd5d07
ali-ubuntu <kholmovaia@stundents.uni-marburg.de>
2024-06-24 17:02:10 +0200

    fix routes

--- backend/agent-microservice/src/app.py
+++ backend/agent-microservice/src/app.py
@@ -11,13 +11,12 @@ from agent_service.parsers.exercises_parser import ExercisesParser
 from flasgger import Swagger
 from agent_service.core.swagger import GERMAN_ANSWER_API, LAW_ANSWER_API, LESSON_API
 from flasgger.utils import swag_from
+from scripts.setup_logging import setup_logging
 # http://localhost:5000/apidocs/ for API docs
 for _ in logging.root.manager.loggerDict:
     logging.getLogger(_).setLevel(logging.CRITICAL)
 
-logging.basicConfig(format='%(asctime)s | %(levelname)s: %(message)s', level=logging.NOTSET)
-logger = logging.getLogger('REACT Agent')
-logger.setLevel(logging.INFO)
+setup_logging()
 warnings.filterwarnings("ignore")
 
 app = FlaskAPI(__name__)
