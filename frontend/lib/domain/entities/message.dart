728069020bbf8da2adff2009ff754da0c3bd82ca
ali-ubuntu <kholmovaia@stundents.uni-marburg.de>
2024-06-25 16:23:46 +0200

    feat decode audio flask

--- frontend/lib/domain/entities/message.dart
+++ frontend/lib/domain/entities/message.dart
@@ -1,3 +1,22 @@
+enum MessageType{
+  message, reading, listening, grammar
+}
+
+extension MessageTypeExtension on MessageType {
+  static MessageType fromString(String type) {
+    switch (type) {
+      case 'Reading':
+        return MessageType.reading;
+      case 'Grammar':
+        return MessageType.grammar;
+      case 'Listening':
+        return MessageType.listening;
+      default:
+        return MessageType.message;
+    }
+  }
+}
+
 class Message {
   final String text;
   final String userID;
