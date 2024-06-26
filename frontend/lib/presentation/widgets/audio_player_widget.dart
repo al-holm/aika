546f08570b9a8e3ddfb837f25ecdd4b77eca71c7
ali-ubuntu <kholmovaia@stundents.uni-marburg.de>
2024-06-26 20:29:25 +0200

    fix audio reset

--- frontend/lib/presentation/widgets/audio_player_widget.dart
+++ frontend/lib/presentation/widgets/audio_player_widget.dart
@@ -4,6 +4,7 @@ import 'package:frontend/styles/app_styles.dart';
 class AudioPlayer extends StatelessWidget {
   final Duration maxDuration;
   final VoidCallback playPauseAudio;
+  final VoidCallback resetAudio;
   final bool isPlaying;
   final Stream<Duration> positionStream;
 
@@ -11,8 +12,9 @@ class AudioPlayer extends StatelessWidget {
     Key? key,
     required this.maxDuration,
     required this.playPauseAudio,
+    required this.resetAudio,
     required this.isPlaying,
-    required this.positionStream
+    required this.positionStream,
   }) : super(key: key);
 
   @override
@@ -20,39 +22,43 @@ class AudioPlayer extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.only(left: 5, right: 5, bottom: 5),
       decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey, width: 1),
-                borderRadius: const BorderRadius.all(Radius.circular(5))
-          ),
+        border: Border.all(color: Colors.grey, width: 1),
+        borderRadius: const BorderRadius.all(Radius.circular(5)),
+      ),
       child: Row(
-      children: [
-        IconButton(
-          icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
-          onPressed: playPauseAudio,
-        ),
-        const SizedBox(width: 10),
-        StreamBuilder<Duration>(
-          stream: positionStream,
-          builder: (context, snapshot) {
-            double progress = 0.0;
-            if (snapshot.hasData && maxDuration.inMilliseconds > 0) {
-              progress = snapshot.data!.inMilliseconds / maxDuration.inMilliseconds;
-            }
-
-            return Container(
-          height: MediaQuery.of(context).size.height * 0.01,
-          width: MediaQuery.of(context).size.width * 0.4,
-          child: CustomPaint(
-            painter: ProgressBarPainter(progress),
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+            onPressed: playPauseAudio,
           ),
-        );
-        }
-        )
-      ],
-    
-    )
+          StreamBuilder<Duration>(
+            stream: positionStream,
+            builder: (context, snapshot) {
+              double progress = 0.0;
+              if (snapshot.hasData && maxDuration.inMilliseconds > 0) {
+                progress = snapshot.data!.inMilliseconds / maxDuration.inMilliseconds;
+              }
+
+              return Container(
+                height: MediaQuery.of(context).size.height * 0.01,
+                width: MediaQuery.of(context).size.width * 0.35,
+                child: CustomPaint(
+                  painter: ProgressBarPainter(progress),
+                ),
+              );
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: resetAudio,
+          ),
+        ],
+      ),
     );
+  }
 }
-}
+
 class ProgressBarPainter extends CustomPainter {
   final double progress;
 
