c91ab26ca1beeda5f02bb6d975eb1018afeb5128
ali-ubuntu <kholmovaia@stundents.uni-marburg.de>
2024-07-11 17:18:28 +0200

    feat added question catalogue

--- frontend/lib/presentation/screens/main_menu_screen.dart
+++ frontend/lib/presentation/screens/main_menu_screen.dart
@@ -5,6 +5,7 @@ import 'package:frontend/presentation/screens/german_chat_screen.dart';
 import 'package:frontend/presentation/screens/law_chat_screen.dart';
 import 'package:frontend/presentation/widgets/main_menu_widgets.dart';
 import 'package:frontend/styles/app_styles.dart';
+
 class MainMenuScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,59 +16,59 @@ class MainMenuScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppStyles.accentColor,
       body: SafeArea(
-        child: _buildMenuContent(context, screenSize, imageDimension, padding)
+          child:
+              _buildMenuContent(context, screenSize, imageDimension, padding)),
+    );
+  }
+
+  Widget _buildMenuContent(BuildContext context, Size screenSize,
+      double imageDimension, double padding) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topRight,
+            padding: EdgeInsets.only(right: padding * 3, top: padding * 2),
+            child: MainMenuSettingButton(
+                onPressed: () => _navigateToSettings(context)),
+          ),
+          MainMenuTitleText(
+            leftPadding: padding * 10,
+            topPadding: screenSize.height * 0.00,
+            bottomPadding: screenSize.height * 0.03,
+          ),
+          Center(
+            child: MainMenuImageContainer(dimension: imageDimension),
+          ),
+          SizedBox(height: screenSize.height * 0.05),
+          MainMenuButton(
+            text: 'DEUTSCH!',
+            onPressed: () => _navigateToGermanChat(context),
+          ),
+          SizedBox(height: screenSize.height * 0.03),
+          MainMenuButton(
+            text: 'RECHT & POLITIK',
+            onPressed: () {
+              _navigateToLawChat(context);
+            },
+          ),
+        ],
       ),
     );
   }
 
-  Widget _buildMenuContent(BuildContext context, Size screenSize, double imageDimension, double padding) {
-    return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                alignment: Alignment.topRight,
-                padding: EdgeInsets.only(right: padding * 3, top: padding * 2),
-                child: MainMenuSettingButton(
-                  onPressed: () => _navigateToSettings(context)
-                  ),
-              ),
-              MainMenuTitleText(
-                leftPadding: padding * 10,
-                topPadding: screenSize.height * 0.00,
-                bottomPadding: screenSize.height * 0.03,
-              ),
-              Center(
-                child: MainMenuImageContainer(dimension: imageDimension),
-              ),
-              SizedBox(height: screenSize.height * 0.05),
-              MainMenuButton(
-                text: 'DEUTSCH!',
-                onPressed: () => _navigateToGermanChat(context),
-              ),
-              SizedBox(height: screenSize.height * 0.03),
-              MainMenuButton(
-                text: 'RECHT & ALLTAG',
-                onPressed: () {
-                   _navigateToLawChat(context);
-                },
-              ),
-            ],
-          ),
-        );
-  }
-  
   _navigateToSettings(BuildContext context) {
     Navigator.pushNamed(context, '/settings');
   }
-  
+
   _navigateToGermanChat(BuildContext context) {
     _navigateToChat(context, 'german');
   }
-  
+
   _navigateToLawChat(BuildContext context) {
-    _navigateToChat(context, 'law'); 
+    _navigateToChat(context, 'law');
   }
 
   _navigateToChat(BuildContext context, String chatID) {
@@ -76,8 +77,9 @@ class MainMenuScreen extends StatelessWidget {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => chatID == 'german' ? GermanChatScreen() : LawChatScreen(),
-        ),
+        builder: (context) =>
+            chatID == 'german' ? GermanChatScreen() : LawChatScreen(),
+      ),
     );
   }
 }
