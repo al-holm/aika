7e3dc13baa98a78887e5a8906ae44a3863961d7e
ali-ubuntu <kholmovaia@stundents.uni-marburg.de>
2024-07-18 17:38:15 +0200

    feat: client restore message history
    - implemented use case FetchMessageHistory
    - integrated sending restore history request on initializing message history
    
    Tests
    tested UI

--- frontend/lib/domain/repositories/chat_repository.dart
+++ frontend/lib/domain/repositories/chat_repository.dart
@@ -1,10 +1,12 @@
 import 'package:frontend/domain/entities/message.dart';
 import 'package:frontend/domain/entities/task.dart';
 
+
 abstract class ChatRepository {
   Future<Message> sendMessage(
     String chatId, Message message);
   Future<void> sendImage(String chatId, String imagePath);
   Future<Message> fetchLesson(String chatId);
   Future<List<Task>> fetchTasks(String chatId);
+  Future<List<Message>> fetchMessageHistory(String chatId);
 }
